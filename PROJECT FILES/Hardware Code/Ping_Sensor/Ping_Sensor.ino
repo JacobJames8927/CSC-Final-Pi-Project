d19b1b5d3ae2947747aeb56c3bfe19f8c0450489
JacobJames8927 <152635266+JacobJames8927@users.noreply.github.com>
2024-05-03 10:16:02 -0500

    Update Ping_Sensor.ino

--- PROJECT FILES/Hardware Code/Ping_Sensor/Ping_Sensor.ino
+++ PROJECT FILES/Hardware Code/Ping_Sensor/Ping_Sensor.ino
@@ -1,4 +1,3 @@
-
 const int trigPin_1 = 13;
 const int trigPin_2 = 8;
 const int echoPin_1 = 12;
@@ -6,7 +5,7 @@ const int echoPin_2 = 10;
 
 int population = 0;
 
-float duration_1, distance_1, duration_2, distance_2;
+float duration_1, distance_1, duration_2, distance_2,past_distance_1, past_distance_2;
 
 void setup() {
   pinMode(trigPin_1, OUTPUT);
@@ -17,6 +16,7 @@ void setup() {
 }
 
 void loop() {
+  
   digitalWrite(trigPin_1, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin_1, HIGH);
@@ -37,18 +37,28 @@ void loop() {
   duration_2 = pulseIn(echoPin_2, HIGH);
   distance_2 = (duration_2*.0343)/2;
     
-  Serial.print("Distance One: ");
-  Serial.println(distance_1);
-  Serial.print("Distance Two: ");
-  Serial.println(distance_2);
+  //Serial.print("Distance One: ");
+  //Serial.println(distance_1);
+  //Serial.print("Distance Two: ");
+  //Serial.println(distance_2);
+  Serial.println(population);
 
   check_for_human(distance_1, distance_2);
-  
+  past_distance_1 = distance_1;
+  past_distance_2 = distance_2;
   delay(50);
 }
 
 float check_for_human(float distance_1, float distance_2) {
-  if (distance_1 <= 15 || distance_2 <= 15) {
-    population += 1;
-  }
+  if (distance_1 != past_distance_1 + 15 || distance_1 != past_distance_1 - 15){
+    if (distance_1 <= 15){
+      population += 1;
+    }
+    
+  };
+  if (distance_2 != past_distance_2 + 15 || distance_2 != past_distance_2 - 15){
+    if (distance_2 <= 15){
+      population += 1;
+    }
+  };
 }
